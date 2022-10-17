0907f2c6724e37e4b2469d721c8581a146f0474f
Matheus de Oliveira <matheus_oliveira8@estudante.sc.senai.br>
2022-10-17 20:06:20 -0300

    Aula12

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,15 +1,79 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <TheHeader v-if="showHeader"/>
+    <h1>Hello World</h1><br>
+    <input v-model="name" type="text">
+    {{ name }} <br>
+
+    <button @click="showHeader = !showHeader">
+      Ativar e Desativar header
+    </button>
+  </div>
+ 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TheHeader from './components/TheHeader'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TheHeader
+  },
+  data(){
+    return{
+      name: 'Matheus',
+      showHeader: true
+    }
+  },
+  // Criação
+    // Preparar o componente
+    // Ajax, inicializar algumas variáveis
+    // Não tem acesso ao DOM
+  //Montagem
+    // Inicializar um lib externa (new Lib())
+    // Precisa de acesso ao template (DOM)
+    // Tem acesso ao template (DOM)
+  // Atualização
+    // Debug
+    // Update
+  // Desmontagem
+    // Remover tudo o que for necessário (lib->destroy())
+    // para liberar memória
+
+  //HOOKS
+  //beforeUpdate(){
+  //console.log('beforeUpdate')
+  //},
+  //updated() {
+  //  console.log('updated')
+  //},
+  //beforeCreate(){
+  //  console.log('beforeCreate')
+  //},
+  //created(){
+  //  console.log('created')
+  //},
+  //beforeMount(){
+  //  console.log('beforeMount')
+  //},
+  //mounted(){
+  //  console.log('mounted')
+  //},
+  //beforeUnmount(){
+  // console.log('beforeUnmount')
+  //},
+  //unmounted(){
+  //  console.log('unmounted')
+  //},
+
+  watch: {
+
+  },
+  computed: {
+
+  },
+  methods: {
+
   }
 }
 </script>
---
+++ app/src/components/TheHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="header">
+    Header
+  </header>
+</template>
+
+<script>
+export default {
+  beforeUnmount(){
+    // Destruir as nossas libs
+    // Eventos
+    // Listeners
+    console.log('beforeUnmount')
+  },
+  unmounted(){
+    console.log('unmounted')
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+  .header{
+    background-color: #000;
+    color: #fff;
+  }
+</style>
