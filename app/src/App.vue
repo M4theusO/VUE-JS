840d40792aee872fe5f965fccec9bf2f58b19acf
Matheus de Oliveira <matheus_oliveira8@estudante.sc.senai.br>
2022-10-20 19:53:30 -0300

    aula17

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,89 +1,10 @@
 <template>
-  <div>
-<!--
-    <PAI> -> props
-      <FILHO>
-      emit - event <- <NETO></NETO>
-      </FILHO>
-    </PAI>
--->  
-      <BaseAlert v-if="showAlert" :variant="variant" @close="onClose()" >
-        {{text}}
-      </BaseAlert>
-    
-  </div>
- 
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
-
-<script>
-import BaseAlert from './components/BaseAlert.vue';
-export default {
-  name: 'App',
-  components: {
-    BaseAlert
-},
-  data(){
-    return{
-      showAlert: true,
-      variant: 'success',
-      text: 'Seu formulário foi enviado com sucesso!',
-    }
-  },
-  // Criação
-    // Preparar o componente
-    // Ajax, inicializar algumas variáveis
-    // Não tem acesso ao DOM
-  //Montagem
-    // Inicializar um lib externa (new Lib())
-    // Precisa de acesso ao template (DOM)
-    // Tem acesso ao template (DOM)
-  // Atualização
-    // Debug
-    // Update
-  // Desmontagem
-    // Remover tudo o que for necessário (lib->destroy())
-    // para liberar memória
-
-  //HOOKS
-  //beforeUpdate(){
-  //console.log('beforeUpdate')
-  //},
-  //updated() {
-  //  console.log('updated')
-  //},
-  //beforeCreate(){
-  //  console.log('beforeCreate')
-  //},
-  //created(){
-  //  console.log('created')
-  //},
-  //beforeMount(){
-  //  console.log('beforeMount')
-  //},
-  //mounted(){
-  //  console.log('mounted')
-  //},
-  //beforeUnmount(){
-  // console.log('beforeUnmount')
-  //},
-  //unmounted(){
-  //  console.log('unmounted')
-  //},
-
-  watch: {
-
-  },
-  computed: {
-
-  },
-  methods: {
-    onClose(){
-      this.showAlert = false
-      console.log('on Close')
-    }
-  }
-}
-</script>
 
 <style>
 #app {
@@ -92,6 +13,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
