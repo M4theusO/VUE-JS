e2bb596776ab6240cdc319bbb9ef39ecde24b71a
Matheus de Oliveira <matheus_oliveira8@estudante.sc.senai.br>
2022-10-20 21:26:03 -0300

    aula18

---
+++ app/src/views/UserEditView.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        {{ $route.params.id }}
+        Usuário
+    </div>
+</template>
+
+<script>
+    export default{
+        created(){
+            console.log(this.$router)
+        }
+    }
+</script>
