e1c984897ee18cd59b15364559eba96eefd6b44a
Matheus de Oliveira <matheus_oliveira8@estudante.sc.senai.br>
2022-10-25 21:41:16 -0300

    aula25

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,29 +1,53 @@
 <template>
-  <AppProduct/>
-  {{ name }}
+  {{ name }} <br><br>
+
+  <h5>User</h5>
+  {{ user.first_name }}
+  {{ user.last_name }}
+  <br><br>
+  <h5>Admin</h5>
+  {{ admin.first_name }}
+  {{ admin.last_name }}
+  <br><br>
   <img @click="changeName()" alt="Vue Logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to your Vue.js App"></HelloWorld>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue';
-import AppProduct from './components/Products/AppProduct.vue';
+import { ref, reactive } from 'vue';
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
-    AppProduct
   },
   setup(){
+    const user = reactive({
+      first_name: 'John',
+      last_name: 'Snow'
+    });
+
+    const admin = ref({
+      first_name: 'Admin',
+      last_name: 'Master'
+    });
+
+    const count = ref('test')
+    console.log(count)
+
     //logica cabulosa
     let name = 'Matheus'
 
     const changeName = () => {
       alert('Chegooou')
       name = 'Matheus Oliveira'
+      user.first_name = 'Sansa'
+      admin.value.first_name = 'XXXX'
     }
     return{
+      user,
+      admin,
       name,
       changeName
     }
